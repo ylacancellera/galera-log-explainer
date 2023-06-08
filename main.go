59e73bbc33c8eda3c7cf38a4afff484c2d0128b5
Yoann La Cancellera <lacancellera.yoann@gmail.com>
2023-06-08 17:26:14 +0200

    Add: version cmd

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -18,6 +19,13 @@ import (
 	"github.com/ylacancellera/galera-log-explainer/utils"
 )
 
+// ldflags
+var (
+	version string
+	commit  string
+	date    string
+)
+
 var CLI struct {
 	NoColor        bool
 	Since          *time.Time      `help:"Only list events after this date, you can copy-paste a date from mysql error log"`
@@ -26,11 +34,12 @@ var CLI struct {
 	PxcOperator    bool            `default:"false" help:"Analyze logs from Percona PXC operator. Off by default because it negatively impacts performance for non-k8s setups"`
 	ExcludeRegexes []string        `help:"Remove regexes from analysis. List regexes using 'galera-log-explainer regex-list'"`
 
-	List      list      `cmd:""`
-	Whois     whois     `cmd:""`
-	Sed       sed       `cmd:""`
-	Ctx       ctx       `cmd:""`
-	RegexList regexList `cmd:""`
+	List      list       `cmd:""`
+	Whois     whois      `cmd:""`
+	Sed       sed        `cmd:""`
+	Ctx       ctx        `cmd:""`
+	RegexList regexList  `cmd:""`
+	Version   versioncmd `cmd:""`
 
 	GrepCmd  string `help:"'grep' command path. Could need to be set to 'ggrep' for darwin systems" default:"grep"`
 	GrepArgs string `help:"'grep' arguments. perl regexp (-P) is necessary. -o will break the tool" default:"-P"`
@@ -53,6 +62,16 @@ func main() {
 	utils.SkipColor = CLI.NoColor
 	err := ctx.Run()
 	ctx.FatalIfErrorf(err)
+}
+
+type versioncmd struct{}
+
+func (v *versioncmd) Help() string {
+	return ""
+}
+func (v *versioncmd) Run() error {
+	fmt.Printf("version: %s, commit:%s, built at %s\n", version, commit, date)
+	return nil
 }
 
 // timelineFromPaths takes every path, search them using a list of regexes
